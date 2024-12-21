995cfa69beaa5bc62c29090810cb3a16df45c516
hadis-soleymani <hadiissoleymani@gmail.com>
2024-12-21 12:21:32 +0330

    add iamge

---
+++ NZWalks.API/Controllers/ImagesController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NZWalks.API.Models.DTO;
+
+namespace NZWalks.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ImagesController : ControllerBase
+    {
+        //POST : api/Images/Upload
+        [HttpPost]
+        [Route("Upload")]
+
+        public async Task<IActionResult> Upload([FromForm] ImageUploadRequestDto request)
+        {
+            ValidateFileUpload(request);
+
+            if (ModelState.IsValid)
+            {
+                //User repository to upload image
+            }
+            return BadRequest(ModelState);
+        }
+
+        private void ValidateFileUpload(ImageUploadRequestDto request)
+        {
+            var allowedExtentions = new string[] { ".jpg", "jpeg", ".png" };
+
+            if (!allowedExtentions.Contains(Path.GetExtension(request.File.FileName)))
+            {
+                ModelState.AddModelError("file", "Unsupported file extention");
+            }
+
+            if (request.File.Length > 10485760)
+            {
+                ModelState.AddModelError("file", "File size more than 10Mb, please upload a smaller size file.");
+            }
+        }
+    }
+}
