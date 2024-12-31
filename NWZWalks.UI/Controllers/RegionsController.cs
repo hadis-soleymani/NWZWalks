0d8008dafeeb910ff73522dafc3c7a9944c23770
hadis-soleymani <hadiissoleymani@gmail.com>
2024-12-31 15:02:57 +0330

    add changes

---
+++ NWZWalks.UI/Controllers/RegionsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NWZWalks.UI.Controllers
+{
+    public class RegionsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
